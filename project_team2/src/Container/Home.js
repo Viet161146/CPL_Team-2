0f9f4efd917ecef3e237929487e5867bc1f846f2
KimChung02 <Chungnkhe160935@fpt.edu.vn>
2024-04-01 10:37:12 +0700

    articles

--- project_team2/src/Container/Home.js
+++ project_team2/src/Container/Home.js
@@ -1,39 +1,28 @@
 import React, { useEffect, useState } from "react";
 import style from "./Home.module.css";
-
+import axios from "axios";
+import moment from "moment";
 const Home = () => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    // Dữ liệu từ đoạn JSON bạn đã cung cấp
-    const data = {
-      "articles": [
-        {
-          "slug": "test",
-          "title": "test",
-          "description": "test",
-          "body": "test",
-          "tagList": [
-            "test"
-          ],
-          "createdAt": "2024-03-29T16:00:41.342Z",
-          "updatedAt": "2024-03-29T16:00:41.342Z",
-          "favorited": true,
-          "favoritesCount": 0,
-          "author": {
-            "username": "chun",
-            "bio": "test",
-            "image": "https://scontent.fhan14-2.fna.fbcdn.net/v/t39.30808-6/434201679_947972393407608_628235543591513876_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=8hVAcEDk9g0AX_84M3G&_nc_ht=scontent.fhan14-2.fna&oh=00_AfDtxHZn3I1fepETmYUBVAA5coT546oNQQslFfsQenOSzw&oe=660BBE39",
-            "following": true
-          }
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.realworld.io/api/articles"
+        );
+        if (response.data && Array.isArray(response.data.articles)) {
+          setArticles(response.data.articles);
+        } else {
+          console.error("Invalid response data");
         }
-      ],
-      "articlesCount": 0
+      } catch (error) {
+        console.error("fetch articles error", error);
+      }
     };
-
-    // Lưu trữ dữ liệu vào state
-    setArticles(data.articles);
+    fetchArticles();
   }, []);
+  //format date
 
   return (
     <div className={style.home}>
@@ -42,20 +31,114 @@ const Home = () => {
           <h1>conduit</h1>
           <p>A place to share your knowledge</p>
         </div>
-        <div className="content">
+        <br />
+        <br />
+
+        <div className="container">
           <div className="row">
             <div className="col-md-9">
-              {articles.map((article, index) => (
-                <div key={index}>
-                  <p>slug: {article.slug}</p>
-                  <p>title: {article.title}</p>
-                  <p>description: {article.description}</p>
-                  <p> name: {article.author.username}</p>
-                  {/* Add more fields if needed */}
+              <div className="container" style={{ width: "80%" }}>
+                <div className="feed-toggle">
+                  <ul className="nav nav-pills outline">
+                    <li className="nav-item">
+                      <a
+                        href=""
+                        style={{ color: "#5cb85c", textDecoration: "none" }}
+                      >
+                        Global Feed
+                      </a>
+                      <hr
+                        style={{
+                          border: "2px solid green",
+                          borderWidth: "4px",
+                        }}
+                      />
+                    </li>
+                  </ul>
                 </div>
-              ))}
+                <div>
+                  {articles.map((article, index) => (
+                    <div key={index}>
+                      <div style={{ display: "flex", alignItems: "center" }}>
+                        <img
+                          style={{ borderRadius: "50%", marginRight: "5px" }}
+                          src={article.author.image}
+                          alt="avt"
+                        />
+                        <div
+                          style={{ display: "flex", flexDirection: "column" }}
+                        >
+                          <span className="author-name">
+                            <span style={{ color: "#5cb85c" }}>
+                              {article.author.username}
+                            </span>
+                            <br />
+                            <span
+                              className="date"
+                              style={{ fontSize: "70%", color: "#BBBBBB" }}
+                            >
+                              {moment(article.createdAt).format("MMMM D, YYYY")}
+                            </span>
+                          </span>
+                        </div>
+                      </div>
+
+                      <h3> {article.title}</h3>
+                      <p style={{ color: "#BBBBBB", fontSize: "80%" }}>
+                        {article.description}
+                      </p>
+                      <div style={{ display: "flex", alignItems: "center" }}>
+                        <span
+                          style={{
+                            marginRight: "10px",
+                            color: "#BBBBBB",
+                            fontSize: "70%",
+                          }}
+                        >
+                          Read more...
+                        </span>
+                        <ul
+                          className="tag-list"
+                          style={{
+                            listStyle: "none",
+                            padding: 0,
+                            marginLeft: "auto",
+                          }}
+                        >
+                          {article.tagList.map((tag, index) => (
+                            <li
+                              key={index}
+                              className="tag-default tag-pill tag-outline"
+                              style={{
+                                marginRight: "5px",
+                                marginBottom: "5px",
+                                padding: "5px 10px",
+                                borderRadius: "20px",
+                                background: "#f0f0f0",
+                                color: "#BBBBBB",
+                                display: "inline-block",
+                                fontSize: "70%",
+                              }}
+                            >
+                              {tag}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                      <hr />
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
-            <div className="col-md-3"></div>
+            <div className="col-md-3">
+              <div className="slidebar">
+                <p>Popular Tags</p>
+                <div className="tag-list">
+                  <a href="">x</a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
